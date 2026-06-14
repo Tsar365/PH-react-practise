c8945aa36e9609ff4395ffdc29473ad76e84ee1b
Ehasanul Haque <ehasanmd365@gmail.com>
2026-06-14 23:41:01 +0600

    36-4 updated

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,6 +1,7 @@
  
 import './App.css'
 
+
 function App() {
   
 
@@ -9,29 +10,63 @@ function App() {
     
           <h1>Get started</h1>
          <Person></Person>
-         <Person></Person>
+         {/* <Person></Person> */}
          <Car></Car>
          <Person></Person>
          <Sports></Sports>
+         <Student></Student>
+         <Student></Student>
+         <Counter name="React" tech="JavaScript" ></Counter>
+         <Counter name="Java" tech="Spring" ></Counter>
+         <Counter></Counter>
      
     </>
   )
 }
 
+function Counter(props) {
+  console.log(props);
+return(
+  <div style={{ 
+    color: 'green',
+     fontSize: '18px'
+      }}>
+    <h3>Counter code:{props.name}</h3>
+  <p>Technology:{props.tech}</p>
+  </div>
+  )
+}
+
 function Person() {
   const age = 20;
+
+const personStyle = {
+  color: 'blue',
+  fontSize: '20px',
+  fontWeight: 'bold',
+};
+
   return (
-   <p>I am a person and I am {age} years old.</p>
+   <p style={personStyle}>I am a person and I am {age} years old.</p>
   );
 }
 
 function Sports(){
   return (
-		<div>
+		<div >
 			<p>I like sports.</p>
 			<h3>Playing code</h3>
 		</div>
 	);
+}
+
+function Student(){
+  return (
+    <div className="student">
+      <p>Name:</p>
+      <p>Dept:</p>
+    </div>
+  )
 }
 
 function Car() {
