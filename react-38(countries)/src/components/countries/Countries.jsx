b28d8fc8272850000686c23c227818df45d2e5a3
Ehasanul Haque <ehasanmd365@gmail.com>
2026-06-19 21:46:13 +0600

    38-7 updated

--- react-38(countries)/src/components/countries/Countries.jsx
+++ react-38(countries)/src/components/countries/Countries.jsx
@@ -1,38 +1,43 @@
 // import React from 'react';
 
-import { use, useState } from "react";
-import Country from "../country/Country";
+import { use, useState } from 'react';
+import Country from '../country/Country';
 import './countries.css';
 
-const Countries = ({countriesPromisesss}) => {
+const Countries = ({ countriesPromisesss }) => {
+	const [visitedCountries, setVisitedCountries] = useState([]);
 
-	const [visitedCountries, setVisitedCountries]=useState([])
-	
-const handleVisitedCountries=(e)=>{
-	console.log("handle visited clicked",e);
-}
+	const handleVisitedCountries = (e) => {
+		console.log('handle visited clicked', e);
+		const newVisitedCountries = [...visitedCountries, e];
+		setVisitedCountries(newVisitedCountries);
+	};
 
-
-
-  const countriesData=use(countriesPromisesss);
-  const countries=countriesData.countries;
-  // const msj=countriesData.message;
-  // const status=countriesData.status;
-  // console.log("countirssdata",countriesData)
-  // console.log("contries",countries)
-  // console.log("messege",msj)
-  // console.log("sta",status)
-  return (
+	const countriesData = use(countriesPromisesss);
+	const countries = countriesData.countries;
+	// const msj=countriesData.message;
+	// const status=countriesData.status;
+	// console.log("countirssdata",countriesData)
+	// console.log("contries",countries)
+	// console.log("messege",msj)
+	// console.log("sta",status)
+	return (
 		<div>
 			<h1>In the countries:{countries.length}</h1>
-			<h3>Total visited: </h3>
+			<h3>Total visited: {visitedCountries.length}</h3>
+
+			<ol>
+				{visitedCountries.map((abcd) => (
+					<li key={abcd.cca3.cca3}>{abcd.name.common}</li>
+				))}
+			</ol>
+
 			<div className="countries">
 				{countries.map((countryyy) => (
 					<Country
 						contry={countryyy}
 						key={countryyy.cca3.cca3}
-						handleVisitedCountriessss={handleVisitedCountries}
-						></Country>
+						handleVisitedCountriessss={handleVisitedCountries}></Country>
 				))}
 			</div>
 		</div>
