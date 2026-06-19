f6b1d545b649854d681e51c381846bcf595a2490
Ehasanul Haque <ehasanmd365@gmail.com>
2026-06-19 12:55:56 +0600

    38-3 updated

--- react-38(countries)/src/components/countries/Countries.jsx
+++ react-38(countries)/src/components/countries/Countries.jsx
@@ -1,19 +1,23 @@
 // import React from 'react';
 
 import { use } from "react";
+import Country from "../country/Country";
 
 const Countries = ({countriesPromisesss}) => {
   const countriesData=use(countriesPromisesss);
   const countries=countriesData.countries;
-  const msj=countriesData.message;
-  const status=countriesData.status;
-  console.log("countirssdata",countriesData)
-  console.log("contries",countries)
-  console.log("messege",msj)
-  console.log("sta",status)
+  // const msj=countriesData.message;
+  // const status=countriesData.status;
+  // console.log("countirssdata",countriesData)
+  // console.log("contries",countries)
+  // console.log("messege",msj)
+  // console.log("sta",status)
   return (
     <div>
       <h1>In the countries:{countries.length}</h1>
+      {
+        countries.map(countryyy=><Country contry={countryyy} key={countryyy.cca3.cca3}></Country>)
+      }
     </div>
   );
 };
--- react-38(countries)/src/components/country/Country.jsx
+++ react-38(countries)/src/components/country/Country.jsx
@@ -1,11 +1,14 @@
 // import React from 'react';
 
-const Country = () => {
+const Country = ({contry}) => {
+  console.log(contry.name.common)
   return (
-    <div>
-      <h3>Name: </h3>
-    </div>
-  );
+		<div>
+      <img src={contry.flags.flags.png} alt={contry.flags.flags.alt}></img>
+			<h3>Name: {contry.name.common}</h3>
+      <p>Population:{contry.population.population}</p>
+		</div>
+	);
 };
 
 export default Country;
