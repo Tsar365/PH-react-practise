5b310830167938ab4bc102ac633ba1965dd52bf8
Ehasanul Haque <ehasanmd365@gmail.com>
2026-06-19 11:52:37 +0600

    38-2 updated

--- react-38(countries)/src/App.jsx
+++ react-38(countries)/src/App.jsx
@@ -1,14 +1,16 @@
 import Countries from './components/countries/Countries'
-
 import './App.css'
+import { Suspense } from 'react'
+
+const countriesPromise = fetch('https://openapi.programming-hero.com/api/all').then(res=> res.json())
 
 function App() {
 
-
   return (
     <>
-      <h1>react on the go...</h1>
-      <Countries></Countries>
+      <Suspense fallback={<p>world tour loading.....</p>}>
+        <Countries countriesPromisesss={countriesPromise}></Countries>
+      </Suspense>
     </>
   )
 }
--- react-38(countries)/src/components/countries/Countries.jsx
+++ react-38(countries)/src/components/countries/Countries.jsx
@@ -1,9 +1,19 @@
 // import React from 'react';
 
-const Countries = () => {
+import { use } from "react";
+
+const Countries = ({countriesPromisesss}) => {
+  const countriesData=use(countriesPromisesss);
+  const countries=countriesData.countries;
+  const msj=countriesData.message;
+  const status=countriesData.status;
+  console.log("countirssdata",countriesData)
+  console.log("contries",countries)
+  console.log("messege",msj)
+  console.log("sta",status)
   return (
     <div>
-      <h1>In the countries</h1>
+      <h1>In the countries:{countries.length}</h1>
     </div>
   );
 };
