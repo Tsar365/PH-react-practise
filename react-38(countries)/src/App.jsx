22371b654d85d3d793f013f6d125e68545529645
Ehasanul Haque <ehasanmd365@gmail.com>
2026-06-19 11:33:05 +0600

    38-1 updated

---
+++ react-38(countries)/src/App.jsx
@@ -0,0 +1,16 @@
+import Countries from './components/countries/Countries'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1>react on the go...</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
